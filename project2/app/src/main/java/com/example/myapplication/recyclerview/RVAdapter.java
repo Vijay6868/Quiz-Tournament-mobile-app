ec86d714778abf4f4f6d5cd161ad5bf059622344
Vijay Kumar <saroyav3@gmail.com>
2024-05-13 20:41:20 +1200

    different card colors functionality added

--- project2/app/src/main/java/com/example/myapplication/recyclerview/RVAdapter.java
+++ project2/app/src/main/java/com/example/myapplication/recyclerview/RVAdapter.java
@@ -1,10 +1,12 @@
 package com.example.myapplication.recyclerview;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
@@ -14,6 +16,8 @@ import java.util.ArrayList;
 
 public class RVAdapter extends RecyclerView.Adapter<RVHolder>{
     private ArrayList<Quiz> quizArrayList;
+    private int[] cardColors = {R.color.bt_color_2, R.color.bt_color_3, R.color.bt_color_1};
+
     //private SelectListener listener;
     public RVAdapter(ArrayList<Quiz> quizArrayList) {
         this.quizArrayList = quizArrayList;
@@ -33,6 +37,12 @@ public class RVAdapter extends RecyclerView.Adapter<RVHolder>{
         holder.qname.setText(item.getQname());
         holder.sdate.setText(item.getSdate());
         holder.edate.setText((item.getEdate()));
+
+        int colorIndex = position % cardColors.length;
+        int colorRes = cardColors[colorIndex];
+
+        holder.rl_color.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), colorRes));
+
     }
 
     @Override
--- project2/app/src/main/java/com/example/myapplication/recyclerview/RVHolder.java
+++ project2/app/src/main/java/com/example/myapplication/recyclerview/RVHolder.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.recyclerview;
 
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,7 @@ import com.example.myapplication.R;
 
 public class RVHolder extends RecyclerView.ViewHolder {
 TextView qname, sdate,edate;
+RelativeLayout rl_color;
 
     public RVHolder(@NonNull View itemView) {
         super(itemView);
@@ -17,6 +19,7 @@ TextView qname, sdate,edate;
         qname = itemView.findViewById(R.id.qname);
         sdate = itemView.findViewById(R.id.start_date);
         edate = itemView.findViewById(R.id.end_date);
+        rl_color = itemView.findViewById(R.id.rl_color);
 
     }
 }
